93f77ba8149952840d5233d8bba95deeda347da2
Jordan Lawler <jordan.lawler@colorado.edu>
2016-03-23 20:08:54 -0600

    model testing for updating standing of an idea complete

--- test/models/idea_standing_test.rb
+++ test/models/idea_standing_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class IdeaStandingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the standing can be upvoted" do
+    Idea.last.update_standing('pos')
+    assert_equal Idea.last.standing.quality, 'aight'
+  end
+
+  test "the standing can be downvoted" do
+    Idea.first.update_standing('neg')
+
+    assert_equal Idea.first.standing.quality, 'aight'
+  end
+
+  test "radical standing cant be upvoted" do
+    Idea.first.update_standing('pos')
+
+    assert_equal Idea.first.standing.quality, 'radical'
+  end
+
+  test "peasant standing cant be downvoted" do
+    Idea.last.update_standing('neg')
+
+    assert_equal Idea.last.standing.quality, 'peasant'
+  end
 end
