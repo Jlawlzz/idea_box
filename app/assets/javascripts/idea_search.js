f4cccd4d539aaede8d8746a0b72c7b282d9a60ab
Jordan Lawler <jordan.lawler@colorado.edu>
2016-03-23 19:28:35 -0600

    search now filters by title, body, and quality

--- app/assets/javascripts/idea_search.js
+++ app/assets/javascripts/idea_search.js
@@ -12,5 +12,17 @@ var filterIdeas = function(){
 }
 
 var matchIdea = function(search, idea) {
+  return (matchTitle(search, idea) || matchBody(search, idea) || matchQuality(search, idea))
+}
+
+var matchTitle = function(search, idea) {
   return $(idea).children('.title-idea').text().substr(0, search.length) == search
+}
+
+var matchBody = function(search, idea) {
+  return $(idea).children('.body-idea').text().substr(0, search.length) == search
+}
+
+var matchQuality = function(search, idea) {
+  return $(idea).children('.idea-quality').children('.quality-idea').text().substr(0, search.length) == search
 }
